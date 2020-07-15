b9faf5736e2ec705e27a508f3cc41652cfcaadf6
Joshua Piper <joshua.g.piper@gmail.com>
2020-07-15 21:05:54 +1000

    generateMarkdown.js : created if/else object for if a user leaves install, titleimage or usage sections blank.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,47 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let installMd =
+    data.install === ""
+      ? "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+      : data.install;
+
+  let titleImageMd =
+    data.titleImage === ""
+      ? ""
+      : `![alt text](${data.titleImage} "${data.title} logo icon")`;
+
+  let usageMd =
+    data.usage === ""
+      ? "Provide instructions and examples for use. Include screenshots as needed."
+      : data.usage;
+
   return `
-  ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
-  ${titleImage}
+  ${titleImageMd}
+  
   # ${data.title}
    *${data.description}*
 
   ## Table of Contents
-  1. Installation
-  2. Usage
-  3. Contributing
-  4. Tests
-  5. Questions/Contact
-  6. License
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Contributing](#contributing)
+  4. [Tests](#tests)
+  5. [Questions/Contact](#questions/contact)
+  6. [License](#license)
 
   ## Installation 
-  ${data.install}
+  ${installMd}
 
   ## Usage
-  ${data.usage}
+  ${usageMd}
 
   ## Contributing
-  ${data.contributing}
+  If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
 
   ## Tests
   ${data.tests}
 
-  ## Any questions?
+  ## Questions/Contact
   ${data.questions}
 
   ## License
