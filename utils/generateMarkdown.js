9711963c3c721e3a9fca3cd144a07f9e59dbb703
Joshua Piper <joshua.g.piper@gmail.com>
2020-07-15 21:27:52 +1000

    created dictionary for - licence: licenceURL.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  let installMd =
-    data.install === ""
-      ? "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
-      : data.install;
-
   let titleImageMd =
     data.titleImage === ""
       ? ""
       : `![alt text](${data.titleImage} "${data.title} logo icon")`;
 
+  let installMd =
+    data.install === ""
+      ? "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+      : data.install;
+
   let usageMd =
     data.usage === ""
       ? "Provide instructions and examples for use. Include screenshots as needed."
       : data.usage;
+
+  let licences = {
+    MIT: "https://choosealicense.com/licenses/mit/",
+    "GNU GPLv3": "https://choosealicense.com/licenses/gpl-3.0/",
+    "Apache 2.0": "https://choosealicense.com/licenses/apache-2.0/",
+    "GNU LGPLv3": "https://choosealicense.com/licenses/lgpl-3.0/",
+    "Mozilla Public License 2.0":
+      "https://choosealicense.com/licenses/mpl-2.0/",
+  };
 
   return `
   ${titleImageMd}
@@ -26,7 +35,7 @@ function generateMarkdown(data) {
   2. [Usage](#usage)
   3. [Contributing](#contributing)
   4. [Tests](#tests)
-  5. [Questions/Contact](#questions/contact)
+  5. [Questions](#questions)
   6. [License](#license)
 
   ## Installation 
@@ -41,7 +50,7 @@ function generateMarkdown(data) {
   ## Tests
   ${data.tests}
 
-  ## Questions/Contact
+  ## Questions
   ${data.questions}
 
   ## License
