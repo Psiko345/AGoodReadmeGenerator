459bc103274ff967a1b7cdc2f0cd4a5385ebf3af
Joshua Piper <joshua.g.piper@gmail.com>
2020-07-15 17:16:45 +1000

    linked generateMarkdown.js, added parameters to writeToFile function, finished questions array.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // array of questions for user
 const questions = [
@@ -31,9 +32,10 @@ const questions = [
     name: "usage",
   },
   {
-    type: "input",
-    message: "",
-    name: "",
+    type: "list",
+    message: "What license do you wish to use?",
+    name: "license",
+    choices: ["MIT", "GNU GPLv3", "Apache 2.0"],
   },
 ];
 
@@ -44,6 +46,9 @@ function writeToFile(fileName, data) {}
 function init() {
   inquirer.prompt(questions).then((response) => {
     console.log(response);
+    let mkdn = generateMarkdown(response);
+    console.log(mkdn);
+    writeToFile("README.md", mkdn);
   });
 }
 
